962b3a3341a2b67560b85def2a0ce6ef03f1cf65
Saneej Palakkal <palakkalsaneej@gmail.com>
2022-05-08 19:34:26 +0530

    Learning React

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import './App.css';
+import SearchIcon from './search.svg';
+
+
+const API_URL = 'http://omdbapi.com/?apikey=ebd0a2c7';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  }
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="Search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {
+        movies?.length > 0
+          ? (<div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+          ) :
+          (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )
+      }
+
+    </div>
+  );
+}
+
+export default App;
