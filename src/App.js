759a43dc1dea12c77386e0dda9f30c4cd84faf41
Saneej Palakkal <palakkalsaneej@gmail.com>
2022-06-04 15:33:28 +0530

    Deployed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 
 
-const API_URL = 'http://omdbapi.com/?apikey=ebd0a2c7';
+const API_URL = 'https://omdbapi.com/?apikey=ebd0a2c7';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
